15e8f1ab6af8c64274a6f645a32c466067d04946
Caitlin Swickard <caitlin6029@gmail.com>
2021-10-04 17:57:41 -0600

    merging conflict

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,15 +1,30 @@
+// import { useEffect, useState } from 'react';
 import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
 import { Posts } from "../../dummyData";
+// import axios from 'axios';
 
 export default function Feed() {
+
+// const [posts,setPosts] =useState([]);
+
+// useEffect(() => {
+//   // console.log("feed rendered");
+//   const fetchPosts = async () => {
+//     const res = await axios.get("/timeline/add user id# here");
+//     setPosts(res.data)
+//     console.log(res);
+//   }
+//   fetchPosts();
+// },[]);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
         {Posts.map((p) => (
-          <Post key={p.id} post={p} />
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
