0b714a7c0eb642ea92337f3ff59decec90690cc2
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-10-09 17:03:36 -0600

    Added CSS
    Updated JSX file with addl code and updated css for navbar changes

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,91 +1,104 @@
 import "./navbar.css";
-import SearchIcon from '@mui/icons-material/Search';
-import PetsIcon from '@mui/icons-material/Pets';
-import ChatIcon from '@mui/icons-material/Chat';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import {Link} from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import PetsIcon from "@mui/icons-material/Pets";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth";
 
+export default function Navbar() {
+  const { user, logout } = useContext(AuthContext);
 
-export default function Navbar(){
+  console.log(user);
 
-    const { user, logout } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
-    console.log(user);
-
-    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-
-    return (
-        <div className="navbarContainer">
-            <div className="navbarLeft">
-                <Link to="/" style={{textDecoration: 'none'}}>
-                <span className="logo">PetSocial</span>
-                </Link>
-            </div>
-            <div className="navbarCenter">
-                <div className="searchbar">
-                    <SearchIcon className="searchIcon" />
-                    <input placeholder="Search for new Furriends" className="searchInput" />
-                </div>
-            </div>
-            <div className="navbarRight">
-                <div className="navbarIcons">
-                    <div className="navbarIconItem">
-                        <PetsIcon />
-                        <span className="navbarIconBadge">1</span>
-                    </div>
-                    <div className="navbarIconItem">
-                        <Link to ="/messenger">
-                            <ChatIcon />
-                        </Link>
-                        <span className="navbarIconBadge">2</span>
-                    </div>
-                    <div className="navbarIconItem">
-                        <NotificationsIcon />
-                        <span className="navbarIconBadge">1</span>
-                    </div>
-                </div>
-                <div className="navbarlinks">
-                    {user ? (
-                        <div>
-                            <div>
-                                <Link to={`/profile/${user.username}`}>
-                                    <img src={
-                                        user.profilePicture
-                                        ? PF + user.profilePicture
-                                        : PF + "pet/noAvatar.png"
-                                    } 
-                                    alt="profile-picture" 
-                                    className="navbarImg" 
-                                    />
-                                </Link>
-                                <span>{user.username}</span>
-                            </div>
-                            <br />
-                            <div>
-                                <Link to="/loginpage" style={{textDecoration: 'none'}} onClick={logout}>
-                                    Logout
-                                </Link>
-                            </div> 
-                        </div>
-                    ) : (
-                        <div>
-                            <div>
-                                <Link to="/loginpage" style={{textDecoration: 'none'}} onClick={logout}>
-                                    Login
-                                </Link>
-                            </div>
-                            <div>
-                                <Link to="/registerpage" style={{textDecoration: 'none'}}>
-                                    Register
-                                </Link> 
-                            </div>
-                        </div>
-   
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="navbarContainer">
+       {user ? (
+           <div>
+           <Link to={`/profile/${user.username}`} style={{marginLeft: "1.5rem" }}>
+           <img
+             src={
+               user.profilePicture
+                 ? PF + user.profilePicture
+                 : PF + "pet/dogcat.jpg"
+             }
+             alt="profile-picture"
+             className="navbarImg"
+           />
+         </Link>
+         <span>{user.username}</span>
+         </div>
+       ) : (
+        <div></div>
+       )} 
+      <div className="navbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">The Social Petwork</span>
+        </Link>
+      </div>
+      <div className="navbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for new Furriends"
+            className="searchInput"
+          />
         </div>
-    );
+      </div>
+      <div className="navbarRight">
+        <div className="navbarIcons">
+          <div className="navbarIconItem">
+            <PetsIcon />
+            <span className="navbarIconBadge">1</span>
+          </div>
+          <div className="navbarIconItem">
+            <Link to="/messenger">
+              <ChatIcon />
+            </Link>
+            <span className="navbarIconBadge">2</span>
+          </div>
+          <div className="navbarIconItem">
+            <NotificationsIcon />
+            <span className="navbarIconBadge">1</span>
+          </div>
+        </div>
+        <div className="navbarlinks">
+          {user ? (
+            <div>
+              <div>
+                <Link
+                  id="link"
+                  to="/loginpage"
+                  style={{ textDecoration: "none", marginLeft: "5rem" }}
+                  onClick={logout}
+                >
+                  Logout
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <div>
+                <Link
+                  to="/loginpage"
+                  style={{ textDecoration: "none" }}
+                  onClick={logout}
+                >
+                  Login
+                </Link>
+              </div>
+              <div>
+                <Link to="/registerpage" style={{ textDecoration: "none" }}>
+                  Register
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
