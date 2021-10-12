8b800cd42cb7129b75eed8023673cca7d9770b92
Caitlin Swickard <caitlin6029@gmail.com>
2021-10-11 18:22:39 -0600

    Caitlin (#52)
    * deploying to heroku
    
    * deploying to heroku
    
    * deploying to heroku
    
    * deploying to heroku

--- server/server.js
+++ server/server.js
@@ -78,9 +78,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
 db.once('open', () => {
   app.listen(PORT, () => {
